fd6cf5c5df50d10747e09cf6ac19dd1efb9fa4dd
willian <willian.l.lucio@gmail.com>
2018-11-06 11:17:13 -0200

    Lesson 18 - Using POST with associations (belongs_to).

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,18 +1,18 @@
 class Contact < ApplicationRecord
-  belongs_to :kind
-  def author
-    "Jackson Pires"
-  end
-
-  def kind_description
-    self.kind.description
-  end
-
-  def as_json(options={})
-    super(
-      root:true,
-      methods: %i(author kind_description),
-      include: { kind: { only: :description } }
-    )
-  end
+  belongs_to :kind#, optional: true
+  # def author
+  #   "Jackson Pires"
+  # end
+  #
+  # def kind_description
+  #   self.kind.description
+  # end
+  #
+  # def as_json(options={})
+  #   super(
+  #     root:true,
+  #     methods: %i(author kind_description),
+  #     include: { kind: { only: :description } }
+  #   )
+  # end
 end
