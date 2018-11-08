91b2c5d5fc4658fe058299febad0c827f88e4608
willian <willian.l.lucio@gmail.com>
2018-11-08 11:09:23 -0200

    Lesson 31 - Associations.

--- app/serializers/contact_serializer.rb
+++ app/serializers/contact_serializer.rb
@@ -1,6 +1,10 @@
 class ContactSerializer < ActiveModel::Serializer
   attributes :id, :name, :email, :birthdate
 
+  belongs_to :kind
+  has_many :phones
+  has_one :address
+
   def attributes(*args)
     h = super(*args)
     h[:birthdate] = object.birthdate.to_time.iso8601 unless self.object.blank?# pt-BR (I18n.l(object.birthdate) )
