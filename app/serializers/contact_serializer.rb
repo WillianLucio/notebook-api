c81d366d6ced7d08ea1e667a758481bd16134e9e
willian <willian.l.lucio@gmail.com>
2018-11-08 11:03:02 -0200

    Lesson 30 - i18n + json_api Adapter.

--- app/serializers/contact_serializer.rb
+++ app/serializers/contact_serializer.rb
@@ -1,3 +1,9 @@
 class ContactSerializer < ActiveModel::Serializer
-  attributes :id
+  attributes :id, :name, :email, :birthdate
+
+  def attributes(*args)
+    h = super(*args)
+    h[:birthdate] = object.birthdate.to_time.iso8601 unless self.object.blank?# pt-BR (I18n.l(object.birthdate) )
+    h
+  end
 end
